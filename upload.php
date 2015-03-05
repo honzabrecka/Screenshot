ea59c7360c6dcc41da4b675eeff30e0910973129
Jan Brecka <jan.brecka@gmail.com>
2015-03-05 19:06:47 +0100

    add outputDir

--- upload.php
+++ upload.php
@@ -1,15 +1,18 @@
 <?php
 
+$outputDir = implode(DIRECTORY_SEPARATOR, ['tests', 'fixtures']);
+
 if (isset($_SERVER['HTTP_X_FILE_NAME']))
 {
-	$filename = $_SERVER['HTTP_X_FILE_NAME'];
-	$input = fopen('php://input', 'rb');
-	$file = fopen($filename, 'wb');
-	stream_copy_to_stream($input, $file);
-	fclose($input);
-	fclose($file);
+  $filename = $_SERVER['HTTP_X_FILE_NAME'];
+  $input = fopen('php://input', 'rb');
+  mkdir($outputDir);
+  $file = fopen(implode(DIRECTORY_SEPARATOR, [$outputDir, $filename]), 'wb');
+  stream_copy_to_stream($input, $file);
+  fclose($input);
+  fclose($file);
 
-	die("done");
+  die("done");
 }
 
 echo 'missing filename';
